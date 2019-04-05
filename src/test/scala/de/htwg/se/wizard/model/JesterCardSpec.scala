f6e59dce92d7b35652750375036e9e59a4a834c9
drescherflo <heidiflorian18>
2019-04-05 16:08:54 +0200

    Reconfigure simple card test to fit into new card model.

--- src/test/scala/de/htwg/se/wizard/model/JesterCardSpec.scala
+++ src/test/scala/de/htwg/se/wizard/model/JesterCardSpec.scala
@@ -2,13 +2,10 @@ package de.htwg.se.wizard.model
 
 import org.scalatest.{Matchers, WordSpec}
 
-class CardSpec extends WordSpec with Matchers{
+class JesterCardSpec extends WordSpec with Matchers{
   "A Card" when {
     "is Jester" should {
-      val jesterCard = Card(null, 0, null)
-      "have value 0" in{
-        jesterCard.number should be(0)
-      }
+      val jesterCard = JesterCard(null)
       "is set" in{
         jesterCard.isJester should be(true)
       }
