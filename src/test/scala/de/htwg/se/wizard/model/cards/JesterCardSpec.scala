2661f8dd77326e306cc7de9893be707b836a2d19
drescherflo <drescherflo@protonmail.com>
2019-04-10 23:00:38 +0200

    Implemented missing tests for JesterCard.

--- src/test/scala/de/htwg/se/wizard/model/cards/JesterCardSpec.scala
+++ src/test/scala/de/htwg/se/wizard/model/cards/JesterCardSpec.scala
@@ -1,13 +1,31 @@
 package de.htwg.se.wizard.model.cards
 
+import de.htwg.se.wizard.model.Player
 import org.scalatest.{Matchers, WordSpec}
 
 class JesterCardSpec extends WordSpec with Matchers{
   "A Card" when {
     "is Jester" should {
       val jesterCard = JesterCard(null)
-      "is set" in{
+      "is a jester" in{
         jesterCard.isJester should be(true)
+      }
+      "is no wizard" in {
+        jesterCard.isWizard should be(false)
+      }
+      "has no color" in {
+        jesterCard.hasColor should be(false)
+      }
+      "has no player as owner" in {
+        jesterCard.hasOwner should be(false)
+      }
+
+      val jesterCardWithPlayer = JesterCard(Player("TestPlayer"))
+      "has an owner" in {
+        jesterCardWithPlayer.hasOwner should be(true)
+      }
+      "has owner 'TestPlayer'" in {
+        jesterCardWithPlayer.owner.name should be("TestPlayer")
       }
     }
   }
