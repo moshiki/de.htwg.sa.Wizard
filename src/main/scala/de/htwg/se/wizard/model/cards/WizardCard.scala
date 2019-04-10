7cc1c451ee3914ec6fa1d46496ebaef436b7f3a8
drescherflo <drescherflo@protonmail.com>
2019-04-10 23:08:29 +0200

    Fix Wizard Card not being a case class.

--- src/main/scala/de/htwg/se/wizard/model/cards/WizardCard.scala
+++ src/main/scala/de/htwg/se/wizard/model/cards/WizardCard.scala
@@ -2,7 +2,7 @@ package de.htwg.se.wizard.model.cards
 
 import de.htwg.se.wizard.model.Player
 
-class WizardCard(owner: Player) extends Card {
+case class WizardCard(owner: Player) extends Card {
   def hasColor: Boolean = false
   def isWizard: Boolean = true
   def isJester: Boolean = false
