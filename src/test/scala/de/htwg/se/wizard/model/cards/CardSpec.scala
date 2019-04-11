10eaa39715e626eaa8ff12402b900b6ec5730f9e
vanessa510 <vanessa.predovic@htwg-konstanz.de>
2019-04-11 14:35:21 +0200

    Test for DefaultCard.

--- src/test/scala/de/htwg/se/wizard/model/cards/CardSpec.scala
+++ src/test/scala/de/htwg/se/wizard/model/cards/CardSpec.scala
@@ -1,7 +1,29 @@
 package de.htwg.se.wizard.model.cards
 
+import de.htwg.se.wizard.model.Player
 import org.scalatest.{Matchers, WordSpec}
 
 class CardSpec extends WordSpec with Matchers {
+  "A Card" when {
+    "is Card" should {
+      val card = DefaultCard("blue", 2, Player("Player"))
+
+      "has Color" in {
+        card.hasColor should be(true)
+      }
+      "is no Wizard" in {
+        card.isWizard should be(false)
+      }
+      "is no Jester" in {
+        card.isJester should be(false)
+      }
+      "number is 2" in {
+        card.number should be(2)
+      }
+
+
+    }
+
+  }
 
 }
