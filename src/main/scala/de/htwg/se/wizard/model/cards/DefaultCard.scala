ed1522724fd85c424f5a383e7242c36e7aee802c
drescherflo <drescherflo@protonmail.com>
2019-04-24 21:30:02 +0200

    Implement a comparison method for DefaultCard. (ISSUE 8)

--- src/main/scala/de/htwg/se/wizard/model/cards/DefaultCard.scala
+++ src/main/scala/de/htwg/se/wizard/model/cards/DefaultCard.scala
@@ -2,11 +2,20 @@ package de.htwg.se.wizard.model.cards
 
 import de.htwg.se.wizard.model.Player
 
-case class DefaultCard(color: String, number: Integer, owner: Player) extends Card {
+case class DefaultCard(color: String, number: Integer, owner: Player) extends Card with Ordered[DefaultCard]{
   require(number >= 1 && number <= 13)
   require(color == "blue" || color == "red" || color == "yellow" || color == "green")
   def hasColor: Boolean = true
   def isWizard: Boolean = false
   def isJester: Boolean = false
   def hasOwner: Boolean = owner != null
+
+  override def compare(that: DefaultCard): Int = this.number - that.number
+
+  override def equals(obj: Any): Boolean = {
+    obj match {
+      case defaultCard: DefaultCard => compare(defaultCard) == 0
+      case _ => false
+    }
+  }
 }
