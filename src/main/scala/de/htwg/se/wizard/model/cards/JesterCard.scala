f5e354ac6f0faacf577a9048e0547d5e43f226bf
drescherflo <drescherflo@protonmail.com>
2019-04-24 23:11:20 +0200

    Updated JesterCard to use Option.

--- src/main/scala/de/htwg/se/wizard/model/cards/JesterCard.scala
+++ src/main/scala/de/htwg/se/wizard/model/cards/JesterCard.scala
@@ -2,14 +2,12 @@ package de.htwg.se.wizard.model.cards
 
 import de.htwg.se.wizard.model.Player
 
-case class JesterCard(owner: Player) extends Card {
+case class JesterCard(owner: Option[Player] = None) extends Card {
   def hasColor: Boolean = false
 
   def isWizard: Boolean = false
 
   def isJester: Boolean = true
-
-  def hasOwner: Boolean = owner != null
 
   override def toString: String = "Jester"
 }
