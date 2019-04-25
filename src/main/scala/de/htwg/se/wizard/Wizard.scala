5bd02202576025afabb043b71645f8c2f6dcdf4a
vanessa510 <vanessa.predovic@htwg-konstanz.de>
2019-04-25 18:04:09 +0200

    Ask for the number of Players and print the number out.

--- src/main/scala/de/htwg/se/wizard/Wizard.scala
+++ src/main/scala/de/htwg/se/wizard/Wizard.scala
@@ -1,10 +1,17 @@
 package de.htwg.se.wizard
 
 import de.htwg.se.wizard.model.Player
+import scala.io.StdIn.readLine
 
 object Wizard {
   def main(args: Array[String]): Unit = {
     val student = Player("Vanessa and Flo")
     println("Welcome to Wizard, a project for SE by " + student)
+
+    print("Give the number of Players: ")
+    val numberOfPlayers = readLine()
+    println("Number of Players: " + numberOfPlayers)
+
+
   }
 }
