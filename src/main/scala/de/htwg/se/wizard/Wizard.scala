3dcbc7ae20bc9d4c525485adae1dffb445746229
drescherflo <drescherflo@protonmail.com>
2019-04-26 12:12:41 +0200

    Wrote test for PlayerRound.

--- src/main/scala/de/htwg/se/wizard/Wizard.scala
+++ src/main/scala/de/htwg/se/wizard/Wizard.scala
@@ -8,35 +8,8 @@ object Wizard {
 
 
   def main(args: Array[String]): Unit = {
-    /*val student = Player("Vanessa and Flo")
-    println("Welcome to Wizard, a project for SE by " + student)
-
-    print("Give the number of Players: ")
-    val numberOfPlayers = getNumberOfPlayers(readInt())
-    println("Number of Players: " + numberOfPlayers)
-
-    println("Please enter your names: ")
-    val names:Array[String] = new Array[String](numberOfPlayers)
-    //val names = Seq()
-
-    for(a <- 1 to numberOfPlayers) {
-      print("Player " + a + " ")
-      val player = Player(readLine())
-      names.update(a-1, player.toString)
-    }
-    println(names.mkString(", "))
-
-    var input = ""
-
-    while(input != "q") {
-      input = readLine()
-    }
-    if(input == "q") {
-      System.exit(1)
-    }*/
-
     val tui = new TUI()
-    tui.run()
+    println(tui.run())
 
 
   }
--- src/test/scala/de/htwg/se/wizard/TUISpec.scala
+++ src/test/scala/de/htwg/se/wizard/TUISpec.scala
@@ -1,21 +1,33 @@
-package de.htwg.se.wizard.model
+package de.htwg.se.wizard
 
-import de.htwg.se.wizard.{TUI, Wizard}
+import de.htwg.se.wizard.model.{CardStack, Player}
 import org.scalatest.{Matchers, WordSpec}
 
-class TUISpec extends WordSpec with Matchers{
+class TUISpec extends WordSpec with Matchers {
   val tui = new TUI()
   "An initialized Game" should {
     "only allow 3 to 6 players" in {
-      for (i <- 3 to 6) {
+      for (i <- 3 to 5) {
         val playerCount = tui.getNumberOfPlayers(i)
         playerCount should be >= 3
         playerCount should be <= 5
       }
     }
     "throw an Exception if to many or less players are wanted" in {
-      an [IllegalArgumentException] should be thrownBy tui.getNumberOfPlayers(2)
-      an [IllegalArgumentException] should be thrownBy tui.getNumberOfPlayers(6)
+      an[IllegalArgumentException] should be thrownBy tui.getNumberOfPlayers(2)
+      an[IllegalArgumentException] should be thrownBy tui.getNumberOfPlayers(6)
+    }
+
+    "get the correct String for each round" in {
+      val players = IndexedSeq(Player("test1"), Player("test2"))
+      val cardStack = CardStack.initialize
+
+      tui.playerTurn(players, 1, 0, cardStack) should startWith
+        """
+           Round 1 - Player 1 (test1)
+           Select one of the following cards:
+        """.stripMargin
+
     }
   }
 }
