56ec7345b2be8b30f7f75a1ce7a3665695b90535
drescherflo <drescherflo@protonmail.com>
2019-05-15 22:00:52 +0200

    Test TUI for max. code coverage.

--- src/test/scala/de/htwg/se/wizard/aview/TUISpec.scala
+++ src/test/scala/de/htwg/se/wizard/aview/TUISpec.scala
@@ -1,7 +1,22 @@
 package de.htwg.se.wizard.aview
 
+import de.htwg.se.wizard.controller.Controller
+import de.htwg.se.wizard.model.RoundManager
 import org.scalatest.{Matchers, WordSpec}
 
 class TUISpec extends WordSpec with Matchers {
-
+  "A Wizard Tui" should {
+    val controller = new Controller(new RoundManager)
+    val tui = new TUI(controller)
+    "register itself in the controller" in {
+        controller.subscribers.contains(tui) should be(true)
+    }
+    "do nothing on input 'q'" in {
+      tui.processInput("q")
+    }
+    "should let the controller evaluate the input" in {
+      tui.processInput("3")
+      controller.getCurrentStateAsString should be("Player 2, please enter your name:")
+    }
+  }
 }
