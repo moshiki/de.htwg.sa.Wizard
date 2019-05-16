fd37734c13e969a98a50b348cdf396adcf15c1cc
drescherflo <drescherflo@protonmail.com>
2019-05-16 17:20:02 +0200

    The points for one round are now stored correctly.

--- src/main/scala/de/htwg/se/wizard/model/ResultTable.scala
+++ src/main/scala/de/htwg/se/wizard/model/ResultTable.scala
@@ -3,5 +3,10 @@ package de.htwg.se.wizard.model
 case class ResultTable(rows: Int = 20, columns: Int = 6) {
   val points: Array[Array[Int]] = Array.fill(rows, columns)(0)
 
-  def updatePoints(round: Int, player: Int, result: Int):Unit = points(round - 1)(player) = result
+  def updatePoints(round: Int, player: Int, result: Int):Unit = {
+    if (round == 1) points(round - 1)(player) = result
+    else points(round - 1)(player) = result + points(round - 2)(player)
+  }
+
+  override def toString: String = super.toString
 }
