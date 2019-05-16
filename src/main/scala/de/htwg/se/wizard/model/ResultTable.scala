7ac82f9d59bc7acb122712176eaa656a35776152
drescherflo <drescherflo@protonmail.com>
2019-05-16 16:52:41 +0200

    Allow the game to update the results in the table.

--- src/main/scala/de/htwg/se/wizard/model/ResultTable.scala
+++ src/main/scala/de/htwg/se/wizard/model/ResultTable.scala
@@ -3,5 +3,5 @@ package de.htwg.se.wizard.model
 case class ResultTable(rows: Int = 20, columns: Int = 6) {
   val points: Array[Array[Int]] = Array.fill(rows, columns)(0)
 
-  def updatePoints(round: Int, player: Int, points: Int):Unit = points(round - 1)(player) = points
+  def updatePoints(round: Int, player: Int, result: Int):Unit = points(round - 1)(player) = result
 }
---
+++ src/test/scala/de/htwg/se/wizard/model/ResultTableSpec.scala
@@ -0,0 +1,23 @@
+package de.htwg.se.wizard.model
+
+import org.scalatest.{Matchers, WordSpec}
+
+class ResultTableSpec extends WordSpec with Matchers{
+  "A ResultTable" should {
+    "initialize itself with an Array of dim 20 * 6 without any parameters" in {
+      val table = ResultTable()
+      table.points.length should be(20)
+      table.points(0).length should be(6)
+    }
+    "initialize itself with an array of dim 3 * 5 when this are the parameters" in {
+      val table = ResultTable(3, 5)
+      table.points.length should be(3)
+      table.points(0).length should be(5)
+    }
+    val table = ResultTable()
+    "update the result correctly in the table" in {
+      table.updatePoints(1, 0, 5)
+      table.points(0)(0) should be(5)
+    }
+  }
+}
