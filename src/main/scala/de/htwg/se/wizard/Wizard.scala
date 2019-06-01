e9f59b8097638a27e056289d753f90cf24f89d71
drescherflo <drescherflo@protonmail.com>
2019-06-01 23:58:23 +0200

    Game should run now.

--- src/main/scala/de/htwg/se/wizard/Wizard.scala
+++ src/main/scala/de/htwg/se/wizard/Wizard.scala
@@ -2,11 +2,12 @@ package de.htwg.se.wizard
 
 import de.htwg.se.wizard.aview.TUI
 import de.htwg.se.wizard.controller.{Controller, RoundManager}
+import de.htwg.se.wizard.model.ResultTable
 
 import scala.io.StdIn.readLine
 
 object Wizard {
-  val controller = new Controller(RoundManager())
+  val controller = new Controller(RoundManager(resultTable = new ResultTable(points = Vector.empty)))
   val tui = new TUI(controller)
   controller.notifyObservers()
 
