77bfd0bfd09e1cfd4a2a36e310db3963c94cce71
vanessa510 <vanessa.predovic@htwg-konstanz.de>
2019-06-04 17:37:03 +0200

    add test for initializing resultTable.

--- src/test/scala/de/htwg/se/wizard/model/ResultTableSpec.scala
+++ src/test/scala/de/htwg/se/wizard/model/ResultTableSpec.scala
@@ -5,20 +5,23 @@ import org.scalatest.{Matchers, WordSpec}
 class ResultTableSpec extends WordSpec with Matchers{
   "A ResultTable" should {
     /*"initialize itself with an Array of dim 20 * 6 without any parameters" in {
-      val table = ResultTable()
+      val table = ResultTable(points = Vector(Vector(10)))
       table.points.length should be(20)
       table.points(0).length should be(6)
+    }*/
+
+    "set ResultTable correctly" in {
+      val table = ResultTable(20, 3, ResultTable.initializeVector(20,3))
+      table.numberOfPlayers should be(3)
+      table.roundsToPlay should be(20)
     }
-    "initialize itself with an array of dim 3 * 5 when this are the parameters" in {
-      val table = ResultTable(3, 5)
-      table.points.length should be(3)
-      table.points(0).length should be(5)
-    }
-    val table = ResultTable()
+
+   /* val table = ResultTable(20,3, ResultTable.initializeVector(20,3))
     "update the result correctly in the first round" in {
       table.updatePoints(1, 0, 5)
       table.points(0)(0) should be(5)
     }
+
     "update the result correctly in all future rounds" in {
       table.updatePoints(2, 0, -3)
       table.points(1)(0) should be(2)
