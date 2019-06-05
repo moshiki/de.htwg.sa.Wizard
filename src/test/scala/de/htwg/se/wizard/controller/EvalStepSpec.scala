aea36ec07aa0938c70563ed000cbe35771fa55e6
vanessa510 <vanessa.predovic@htwg-konstanz.de>
2019-06-05 20:53:45 +0200

    deleted EvalStep tests and add UndoManagerSpec.

--- src/test/scala/de/htwg/se/wizard/controller/EvalStepSpec.scala
+++ src/test/scala/de/htwg/se/wizard/controller/EvalStepSpec.scala
@@ -1,21 +1,22 @@
 package de.htwg.se.wizard.controller
 
+import de.htwg.se.wizard.controller.EvalStep
 import de.htwg.se.wizard.model.ResultTable
 import org.scalatest.{Matchers, WordSpec}
 
 class EvalStepSpec extends WordSpec with Matchers {
   "An EvalStep" when {
-    val resultTable = ResultTable(20, 3, ResultTable.initializeVector(3, 3))
-    val roundManager = RoundManager(resultTable = resultTable)
+   /* val resultTable = ResultTable(20, 3, ResultTable.initializeVector(3, 3))
+    val roundManager = RoundManager(resultTable = resultTable, currentPlayer = 2)
     val controller = new Controller(roundManager)
     var memento: (RoundManager, ControllerState) = (controller.roundManager.copy(), controller.state)
 
     "do undoStep" in {
-      val newMemento = (controller.roundManager.copy(), controller.state)
-      controller.roundManager should be(memento._1)
-      controller.state should be(memento._2)
-      memento should be(newMemento)
+
+     controller.roundManager = controller.roundManager.copy(currentPlayer = 3)
+      controller.undo()
+      controller.roundManager.currentPlayer should not be(3)
     }
-  }
+  }*/
 
 }
---
+++ src/test/scala/de/htwg/se/wizard/util/UndoManagerSpec.scala
@@ -0,0 +1,9 @@
+package de.htwg.se.wizard.util
+
+import org.scalatest.{Matchers, WordSpec}
+
+class UndoManagerSpec extends WordSpec with Matchers{
+    "An UndoManager" when {
+
+    }
+}
