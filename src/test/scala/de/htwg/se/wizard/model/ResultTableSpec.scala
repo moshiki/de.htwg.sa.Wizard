44bfef70521d961c0f0eb5b70d61332f656d4937
vanessa510 <vanessa.predovic@htwg-konstanz.de>
2019-06-05 14:13:01 +0200

    fixed tests in ResultTable.

--- src/test/scala/de/htwg/se/wizard/model/ResultTableSpec.scala
+++ src/test/scala/de/htwg/se/wizard/model/ResultTableSpec.scala
@@ -11,26 +11,25 @@ class ResultTableSpec extends WordSpec with Matchers{
       table.roundsToPlay should be(20)
     }
 
-    /*val table = ResultTable(20,3, ResultTable.initializeVector(3,3))
+    val table = ResultTable(20,3, ResultTable.initializeVector(3,3))
     "update the result correctly in the first round" in {
 
-      table.updatePoints(1, 0, 10)
-      table.points(0)(0)should be(10)
+     val newTable = table.updatePoints(1, 0, 10)
+      newTable.points(0)(0)should be(10)
     }
 
     "update the result correctly in all future rounds" in {
-      table.updatePoints(2, 0, -20)
-      table.points(1)(0) should be(-10)
-    }*/
+      val newTable = table.updatePoints(2, 0, -20)
+      newTable.points(1)(0) should be(-10)
+    }
 
-    //result is not being updated
     "have a nice string representation" in {
       val printTable = ResultTable(2,3,ResultTable.initializeVector(20,3))
-      printTable.updatePoints(1,1,5)
-      printTable.toString should be(
+      val newPrintTable = printTable.updatePoints(1,1,5)
+      newPrintTable.toString should be(
         """|#  Player  1  #  Player  2  #  Player  3  #
            |###########################################
-           |#      0      #      0      #      0      #
+           |#      0      #      5      #      0      #
            |###########################################
            |#      0      #      0      #      0      #
            |###########################################"""
