35909b4525fb320756a23cef84d92c840c058f78
drescherflo <drescherflo@protonmail.com>
2019-06-05 21:49:14 +0200

    Reaction on click on a label with image.

---
+++ src/main/scala/de/htwg/se/wizard/aview/gui/InGamePanel.scala
@@ -0,0 +1,48 @@
+package de.htwg.se.wizard.aview.gui
+
+import de.htwg.se.wizard.controller.{Controller, RoundManager}
+import de.htwg.se.wizard.model.Player
+import de.htwg.se.wizard.model.cards.Card
+import javax.swing.ImageIcon
+
+import scala.swing._
+import Swing._
+import scala.collection.mutable.ListBuffer
+import scala.swing.event.{ButtonClicked, MouseClicked}
+
+class InGamePanel(controller: Controller) extends BoxPanel(Orientation.Vertical) {
+  /*val roundManager:RoundManager = controller.roundManager
+  contents += new BoxPanel(Orientation.Horizontal) {
+    val currentPlayer:Player = roundManager.players(roundManager.currentPlayer)
+    contents += new Label("Round " + roundManager.currentRound)
+    contents += HGlue
+    contents += new Label("Player: " + currentPlayer)
+  }
+
+  val playedCards: List[Card] = roundManager.playedCards*/
+
+  val card0:Label = new Label {
+    private val temp = new ImageIcon("src/main/resources/cards/test.png").getImage
+    private val resize = temp.getScaledInstance(150, 200, java.awt.Image.SCALE_SMOOTH)
+    icon = new ImageIcon(resize)
+    listenTo(mouse.clicks)
+    reactions += {
+      case e: MouseClicked => println("Click 2")
+    }
+  }
+
+  contents += card0
+
+  listenTo(card0)
+
+
+
+
+  /*contents += new Label{
+    private val temp = new ImageIcon("src/main/resources/cards/test.png").getImage
+    private val resize = temp.getScaledInstance(150, 200, java.awt.Image.SCALE_SMOOTH)
+    icon = new ImageIcon(resize)
+  }*/
+}
+
+// TODO: Font: Herculanum
