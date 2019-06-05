862ee15ff0498846a2e35e9d9751aa36a6f5e95d
vanessa510 <vanessa.predovic@htwg-konstanz.de>
2019-06-05 20:36:20 +0200

    add test in EvalStep for undoStep.

--- src/test/scala/de/htwg/se/wizard/controller/EvalStepSpec.scala
+++ src/test/scala/de/htwg/se/wizard/controller/EvalStepSpec.scala
@@ -1,8 +1,21 @@
 package de.htwg.se.wizard.controller
 
-import de.htwg.se.wizard.model.Player
+import de.htwg.se.wizard.model.ResultTable
 import org.scalatest.{Matchers, WordSpec}
 
-class EvalStepSpec {
+class EvalStepSpec extends WordSpec with Matchers {
+  "An EvalStep" when {
+    val resultTable = ResultTable(20, 3, ResultTable.initializeVector(3, 3))
+    val roundManager = RoundManager(resultTable = resultTable)
+    val controller = new Controller(roundManager)
+    var memento: (RoundManager, ControllerState) = (controller.roundManager.copy(), controller.state)
+
+    "do undoStep" in {
+      val newMemento = (controller.roundManager.copy(), controller.state)
+      controller.roundManager should be(memento._1)
+      controller.state should be(memento._2)
+      memento should be(newMemento)
+    }
+  }
 
 }
