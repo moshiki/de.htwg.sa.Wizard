460b394d9d7cfba02d82d8992cca55f670ac25c7
drescherflo <drescherflo@protonmail.com>
2019-06-08 01:27:46 +0200

    Move RoundStrategy test to correct package.

--- src/test/scala/de/htwg/se/wizard/controller/RoundStrategySpec.scala
+++ src/test/scala/de/htwg/se/wizard/controller/RoundStrategySpec.scala
@@ -1,28 +1,26 @@
-package de.htwg.se.wizard.model
+package de.htwg.se.wizard.controller
 
-import de.htwg.se.wizard.controller.{RoundManager, RoundStrategy}
 import org.scalatest.{Matchers, WordSpec}
 
 class RoundStrategySpec extends WordSpec with Matchers{
   "A RoundStrategy" when {
-    /*"should set rounds based on players input" should {
-      val roundManager = RoundManager()
-      val roundManager1 = RoundStrategy.execute(3,roundManager)
+    "should set rounds based on players input" should {
+      val roundManager1 = RoundStrategy.execute(3)
       "with three players" in {
         roundManager1.numberOfPlayers should be(3)
         roundManager1.numberOfRounds should be(20)
       }
-      val roundManager2 = RoundStrategy.execute(4,roundManager)
+      val roundManager2 = RoundStrategy.execute(4)
       "with four Players" in {
         roundManager2.numberOfPlayers should be(4)
         roundManager2.numberOfRounds should be (15)
       }
-      val roundManager3 = RoundStrategy.execute(5, roundManager)
+      val roundManager3 = RoundStrategy.execute(5)
       "with five Players" in {
         roundManager3.numberOfPlayers should be(5)
         roundManager3.numberOfRounds should be(12)
       }
-    }*/
+    }
 
 
   }
