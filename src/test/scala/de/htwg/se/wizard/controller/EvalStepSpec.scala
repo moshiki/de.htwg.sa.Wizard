f64088a86bfd6ede8441ba7937b554489d372825
drescherflo <drescherflo@protonmail.com>
2019-06-07 11:27:53 +0200

    Fix EvalStep tests to match refactoring.

--- src/test/scala/de/htwg/se/wizard/controller/EvalStepSpec.scala
+++ src/test/scala/de/htwg/se/wizard/controller/EvalStepSpec.scala
@@ -6,18 +6,31 @@ import org.scalatest.{Matchers, WordSpec}
 
 class EvalStepSpec extends WordSpec with Matchers {
   "An EvalStep" when {
-    /* val resultTable = ResultTable(20, 3, ResultTable.initializeVector(3, 3))
-    val roundManager = RoundManager(resultTable = resultTable, currentPlayer = 2)
-    val controller = new Controller(roundManager)
-    var memento: (RoundManager, ControllerState) = (controller.roundManager.copy(), controller.state)
-
-    "do undoStep" in {
-
-     controller.roundManager = controller.roundManager.copy(currentPlayer = 3)
-      controller.undo()
-      controller.roundManager.currentPlayer should not be(3)
+    val controller = new Controller(RoundManager(resultTable = ResultTable(points = ResultTable.initializeVector())))
+    val evalStep = new EvalStep(controller)
+    "saves the current controller's state and round manager" in {
+      val state = (controller.roundManager, controller.state)
+      evalStep.doStep()
+      evalStep.memento should be(state)
     }
-  }*/
 
+    "restore the previous state and save the current one" in {
+      val oldState = (controller.roundManager.copy(), controller.state)
+      controller.state = InGameState(controller)
+      val newState = (controller.roundManager.copy(), controller.state)
+      evalStep.undoStep()
+      evalStep.memento should be(newState)
+      controller.roundManager should be(oldState._1)
+      controller.state should be(oldState._2)
+    }
+
+    "revert the undo command" in {
+      val afterUndoState = (controller.roundManager.copy(), controller.state)
+      val beforeUndoState = evalStep.memento
+      evalStep.redoStep()
+      evalStep.memento should be(afterUndoState)
+      controller.roundManager should be(beforeUndoState._1)
+      controller.state should be(beforeUndoState._2)
+    }
   }
 }
