0d00e175356fac36aa4c4a1521f8521bdd765db2
vanessa510 <vanessa.predovic@htwg-konstanz.de>
2019-06-20 22:56:44 +0200

    fixed TUI-tests.

--- src/test/scala/de/htwg/se/wizard/aview/gui/SwingGuiSpec.scala
+++ src/test/scala/de/htwg/se/wizard/aview/gui/SwingGuiSpec.scala
@@ -5,7 +5,8 @@ import de.htwg.se.wizard.model.modelComponent.cards.WizardCard
 import de.htwg.se.wizard.model.modelComponent.{Player, ResultTable}
 import org.scalatest.{Matchers, WordSpec}
 
-/*class SwingGuiSpec extends WordSpec with Matchers{
+class SwingGuiSpec extends WordSpec with Matchers{
+
   val controller = new Controller(RoundManager(resultTable = ResultTable(points = ResultTable.initializeVector())))
   "A SwingGuiSpec" should {
     "load the correct Panel" when {
@@ -32,4 +33,4 @@ import org.scalatest.{Matchers, WordSpec}
     }
   }
 
-}*/
+}
