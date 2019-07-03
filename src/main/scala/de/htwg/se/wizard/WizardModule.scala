29aecdcadc20b07ec3e95eb42b80ac18289901e5
drescherflo <drescherflo@protonmail.com>
2019-07-03 11:46:47 +0200

    Introduce the ModelInterface to the Dependency Injection.

--- src/main/scala/de/htwg/se/wizard/WizardModule.scala
+++ src/main/scala/de/htwg/se/wizard/WizardModule.scala
@@ -1,6 +1,7 @@
 package de.htwg.se.wizard
 
 import com.google.inject.AbstractModule
+import de.htwg.se.wizard.model.ModelInterface
 import de.htwg.se.wizard.model.fileIOComponent.FileIOInterface
 import de.htwg.se.wizard.model.modelComponent.{ResultTable, RoundManager}
 import net.codingwell.scalaguice.ScalaModule
@@ -8,7 +9,7 @@ import de.htwg.se.wizard.model.fileIOComponent._
 
 class WizardModule extends AbstractModule with ScalaModule{
   override def configure(): Unit = {
-    bind[RoundManager].toInstance(RoundManager(resultTable = ResultTable.initializeTable()))
+    bind[ModelInterface].toInstance(RoundManager(resultTable = ResultTable.initializeTable()))
     bind[FileIOInterface].to[FileIOXML.FileIO]
   }
 }
