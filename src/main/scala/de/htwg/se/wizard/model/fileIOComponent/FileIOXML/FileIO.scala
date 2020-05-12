4f5686e4027a2fad0b5c767b7b3e3d84dfaf5ab9
drescherflo <drescherflo@protonmail.com>
2020-05-12 14:56:09 +0200

    FileIO XML: Use Try for better error handling.

--- src/main/scala/de/htwg/se/wizard/model/fileIOComponent/FileIOXML/FileIO.scala
+++ src/main/scala/de/htwg/se/wizard/model/fileIOComponent/FileIOXML/FileIO.scala
@@ -1,27 +1,37 @@
 package de.htwg.se.wizard.model.fileIOComponent.FileIOXML
 
-//case class FileIO() extends FileIOInterface {
-//  override def load(modelInterface: ModelInterface): (String, ModelInterface) = {
-//    val saveState = scala.xml.XML.loadFile("WizardSaveGame.xml")
-//    val controllerStateString = (saveState \ "state").text.trim
-//    val state = controllerStateString
-//
-//    val newRoundManager = modelInterface.fromXML((saveState \ "RoundManager").head)
-//    (state, newRoundManager)
-//  }
-//
-//  override def save(controllerState: String, modelInterface: ModelInterface): Unit = {
-//    def gameToXML: Elem = {
-//      <Game>
-//        <state>
-//          {controllerState}
-//        </state>{modelInterface.toXML}
-//      </Game>
-//    }
-//
-//    import java.io._
-//    val pw = new PrintWriter(new File("WizardSaveGame.xml"))
-//    pw.write(gameToXML.toString())
-//    pw.close()
-//  }
-//}
+import de.htwg.se.wizard.model.fileIOComponent.FileIOInterface
+import de.htwg.se.wizard.model.modelComponent.ModelInterface
+
+import scala.util.Try
+import scala.xml.Elem
+
+case class FileIO() extends FileIOInterface {
+  override def load(modelInterface: ModelInterface): Try[(String, ModelInterface)] = {
+    Try {
+      val saveState = scala.xml.XML.loadFile("WizardSaveGame.xml")
+      val controllerStateString = (saveState \ "state").text.trim
+      val state = controllerStateString
+
+      val newRoundManager = modelInterface.fromXML((saveState \ "RoundManager").head)
+      (state, newRoundManager)
+    }
+  }
+
+  override def save(controllerState: String, modelInterface: ModelInterface): Try[Unit] = {
+    def gameToXML: Elem = {
+      <Game>
+        <state>
+          {controllerState}
+        </state>{modelInterface.toXML}
+      </Game>
+    }
+
+    import java.io._
+    Try {
+      val pw = new PrintWriter(new File("WizardSaveGame.xml"))
+      pw.write(gameToXML.toString())
+      pw.close()
+    }
+  }
+}
--- src/test/scala/de/htwg/se/wizard/model/fileIOComponent/FileIOXML/FileIOSpec.scala
+++ src/test/scala/de/htwg/se/wizard/model/fileIOComponent/FileIOXML/FileIOSpec.scala
@@ -1,17 +1,20 @@
 package de.htwg.se.wizard.model.fileIOComponent.FileIOXML
 
+import de.htwg.se.wizard.controller.controllerComponent.controllerBaseImpl.Controller
+import de.htwg.se.wizard.model.modelComponent.modelBaseImpl.RoundManager
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-class FileIOSpec extends AnyWordSpec with Matchers{
-  /*"An XML File IO" should {
+class FileIOSpec extends AnyWordSpec with Matchers {
+  "An XML File IO" should {
     "save and restore the whole game" in {
       val roundManager = RoundManager.Builder().withNumberOfPlayers(3).withNumberOfRounds(3).build()
       val fileIO = FileIO()
       val controller = new Controller(roundManager, fileIO)
       fileIO.save(controller.controllerStateAsString, roundManager)
       val res = fileIO.load(roundManager)
-      res._2 should be(roundManager)
+      res.get._1 should be("PreSetupState")
+      res.get._2 should be(roundManager)
     }
-  }*/
+  }
 }
