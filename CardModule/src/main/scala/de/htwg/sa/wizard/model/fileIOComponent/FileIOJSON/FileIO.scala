a2a435ed5ae07a02908eb7d9cb54c038c61d0a53
moshiki <felix.g.maier@web.de>
2020-05-20 09:53:52 +0200

    added FileIO to CardModule

--- CardModule/src/main/scala/de/htwg/sa/wizard/model/fileIOComponent/FileIOJSON/FileIO.scala
+++ CardModule/src/main/scala/de/htwg/sa/wizard/model/fileIOComponent/FileIOJSON/FileIO.scala
@@ -2,28 +2,28 @@ package de.htwg.sa.wizard.model.fileIOComponent.FileIOJSON
 
 import java.io.{File, PrintWriter}
 
+import de.htwg.sa.wizard.model.cardComponent.CardInterface
 import de.htwg.sa.wizard.model.fileIOComponent.FileIOInterface
-import de.htwg.sa.wizard.model.resultTableComponent.ResultTableInterface
 import play.api.libs.json.Json
 
 import scala.io.Source
 import scala.util.Try
 
 case class FileIO() extends FileIOInterface {
-  override def load(resultTableInterface: ResultTableInterface, path: String): Try[ResultTableInterface] = {
+  override def load(cardInterface: CardInterface, path: String): Try[CardInterface] = {
     Try{
       val source = Source.fromFile(path + ".json")
       val string = source.getLines().mkString
       source.close
       val json = Json.parse(string)
-      resultTableInterface.fromJson(json)
+      cardInterface.fromJson(json)
     }
   }
 
-  override def save(resultTableInterface: ResultTableInterface, path: String): Try[Unit] = {
+  override def save(cardInterface: CardInterface, path: String): Try[Unit] = {
     Try{
       val printWriter = new PrintWriter(new File(path + ".json"))
-      printWriter.write(Json.prettyPrint(resultTableInterface.toJson))
+      printWriter.write(Json.prettyPrint(cardInterface.toJson))
       printWriter.close()
     }
   }
