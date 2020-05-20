65626e2980c6b5f914da1f899245bee5f1c4b4ba
moshiki <felix.g.maier@web.de>
2020-05-20 10:23:41 +0200

    added FileIOSpec to CardModule

---
+++ CardModule/src/test/scala/de/htwg/sa/wizard/model/cardComponent/fileIOComponent/FileIOJSON/FileIOSpec.scala
@@ -0,0 +1,58 @@
+package de.htwg.sa.wizard.model.cardComponent.fileIOComponent.FileIOJSON
+
+import java.nio.file.{Files, Path}
+
+import de.htwg.sa.wizard.model.cardComponent.CardInterface
+import de.htwg.sa.wizard.model.fileIOComponent.FileIOJSON.FileIO
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import play.api.libs.json.Json
+
+import scala.io.Source
+import scala.util.Success
+
+class FileIOSpec extends AnyWordSpec with Matchers with MockFactory {
+  "A JSON File IO" when {
+    "saving the Card Interface" should {
+      val cardInterfaceStub = stub[CardInterface]
+      val expectedJson = Json.obj("CardInterfaceJson" -> "Test-Data")
+      (cardInterfaceStub.toJson _).when().returns(expectedJson)
+      val fileIO = FileIO()
+      val path = "CardInterface"
+      val result = fileIO.save(cardInterfaceStub, path)
+
+      "ask the Card Interface for its Json data" in {
+        (cardInterfaceStub.toJson _).verify()
+      }
+
+      "store the correct data in the correct path" in {
+        val source = Source.fromFile(path + ".json")
+        val fileContents = source.getLines.mkString
+        source.close()
+        val json = Json.parse(fileContents)
+        json should be(expectedJson)
+        Files.deleteIfExists(Path.of(path + ".json")) should be(true)
+      }
+
+      "return a Success" in {
+        result.isInstanceOf[Success[Unit]] should be(true)
+      }
+    }
+
+    "loading the Card Interface" should {
+      "return the correct Card Interface in a success" in {
+        val path = "CardModule/src/test/resources/CardInterface"
+        val expectedJson = Json.obj("CardInterfaceJson" -> "Test-Data")
+        val expectedCardInterface = stub[CardInterface]
+        val cardInterfaceStub = stub[CardInterface]
+        (cardInterfaceStub.fromJson _).when(expectedJson).returns(expectedCardInterface)
+        val fileIO = FileIO()
+        val result = fileIO.load(cardInterfaceStub, path)
+
+        result.isInstanceOf[Success[CardInterface]] should be(true)
+        result.get should be(expectedCardInterface)
+      }
+    }
+  }
+}
---
+++ CardModule/src/test/scala/de/htwg/sa/wizard/model/cardComponent/fileIOComponent/FileIOXML/FileIOSpec.scala
@@ -0,0 +1,53 @@
+package de.htwg.sa.wizard.model.cardComponent.fileIOComponent.FileIOXML
+
+import java.nio.file.{Files, Path}
+
+import de.htwg.sa.wizard.model.cardComponent.CardInterface
+import de.htwg.sa.wizard.model.fileIOComponent.FileIOXML.FileIO
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+import scala.xml.{Elem, XML}
+
+class FileIOSpec extends AnyWordSpec with Matchers with MockFactory {
+  "An XML FileIO" when {
+    "saving the Card Interface" should {
+      val cardInterfaceStub = stub[CardInterface]
+      val expectedXML: Elem = <CardInterface>testData</CardInterface>
+      (cardInterfaceStub.toXML _).when().returns(expectedXML)
+      val fileIO = FileIO()
+      val path = "CardInterface"
+      val result = fileIO.save(cardInterfaceStub, path)
+
+      "ask the cardInterface for its data in XML" in {
+        (cardInterfaceStub.toXML _).verify()
+      }
+
+      "store the correct data in the correct file" in {
+        val storedXML = XML.loadFile(path + ".xml")
+        storedXML should be(expectedXML)
+        Files.deleteIfExists(Path.of(path + ".xml")) should be(true)
+      }
+
+      "return a Success" in {
+        result.isSuccess should be(true)
+      }
+    }
+
+    "loading the Card Interface" should {
+      "return the correct Card Interface in a success" in {
+        val path = "CardInterface/src/test/resources/CardInterface"
+        val expectedXML: Elem = <CardInterface>testData</CardInterface>
+        val expectedCardInterface = stub[CardInterface]
+        val cardInterfaceStub = stub[CardInterface]
+        (cardInterfaceStub.fromXML _).when(expectedXML).returns(expectedCardInterface)
+        val fileIO = FileIO()
+        val result = fileIO.load(cardInterfaceStub, path)
+
+        result.isSuccess should be(true)
+        result.get should be(expectedCardInterface)
+      }
+    }
+  }
+}
