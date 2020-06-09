044f971a1d4f88a16737cdab64336de8718d61f1
moshiki <felix.g.maier@web.de>
2020-06-10 01:57:44 +0200

    fixed Dockerfile.CardModule changed build-docker-container tags to same name convention

--- build-docker-containers.sh
+++ build-docker-containers.sh
@@ -2,6 +2,6 @@ sbt assembly
 docker build . -t wizard-root
 docker build ./ResultTableModule -t wizard-resulttablemodule
 docker build ./CardModule -t wizard-cardmodule
-docker build -f ./DBDockerfiles/MySQL/Dockerfile.CardModule . -t wizard-mysql-cardmodule
-docker build -f ./DBDockerfiles/MySQL/Dockerfile.RootModule . -t wizard-mysql-root
-docker build -f ./DBDockerfiles/MySQL/Dockerfile.ResultTableModule . -t wizard-mysql-resulttablemodule
+docker build -f ./DBDockerfiles/MySQL/Dockerfile.CardModule . -t wizard-cardmodule-mysql
+docker build -f ./DBDockerfiles/MySQL/Dockerfile.RootModule . -t wizard-root-mysql
+docker build -f ./DBDockerfiles/MySQL/Dockerfile.ResultTableModule . -t wizard-resulttablemodule-mysql
